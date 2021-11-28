c12fa568df500d0ac78ef2f8c02e4bf474a9e46c
Smktd <95113225+CantCode023@users.noreply.github.com>
2021-11-28 10:02:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const button = document.getElementById("btn")
+const button1 = document.getElementById("btn1")
+
+button.onclick = () => {
+    window.open("https://twitter.com/CantCode3", "_blank")
+}
+
+button1.onclick = () => {
+    window.open("https://www.youtube.com/channel/UCH1I4Ak44uDbI67T6i-u1Mg", "_blank")
+}
