53908d797442b08eb416dc6d2c4506d36354c228
Cant Code <95113225+CantCode023@users.noreply.github.com>
2021-11-28 13:34:29 +0000

    Updated script.js file.

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-const button = document.getElementById("btn")
-const button1 = document.getElementById("btn1")
-
-button.onclick = () => {
-    window.open("https://twitter.com/CantCode3", "_blank")
-}
-
-button1.onclick = () => {
-    window.open("https://www.youtube.com/channel/UCH1I4Ak44uDbI67T6i-u1Mg", "_blank")
-}
+console.log("I cant code :c")
